919658450c7614c368ded94c4e0d4e77c2efcc16
bushuev-alex <bushuev-alex@mail.ru>
2023-03-08 23:38:28 +0700

    1. add logging log.txt 2. add function parse message with regex 3. remove timeseries button from inlinebuttons 4. exchange or get_timeseries features are in dependence of regex parsing of message.text

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import telebot
-from config import TOKEN
-from extensions import CryptoConverter, APIException
+from config import TOKEN, START_HELP_TEXT
+from extensions import CryptoConverter, parse_message_text
+from exceptions import APIException
 import redis
 import logging
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 # Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+logging.basicConfig(filename='log.txt',
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 red = redis.Redis(host='localhost', port=6379)
@@ -19,21 +18,17 @@ bot = telebot.TeleBot(TOKEN)
 symbols = CryptoConverter.get_currencies()
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message: telebot.types.Message):
-    text = f"HELLO, {message.chat.username}!\n" \
-           "I'm ChatBot where You can convert currencies!\n\n" \
-           "Insert currencies to exchange in following format (in one line): \n" \
-           "<currency to get> " \
-           "<currency to exchange> " \
-           "<amount to get>\n\n" \
-           "Example:\n\nEUR USD 1\n\n"
+    print(f"User {message.from_user.username} joined the bot")
+    logger.info(f"User {message.from_user.username} joined the bot")
+    text = START_HELP_TEXT.format(message.chat.username)
     bot.reply_to(message, text)
     inline_keyboards(message)
 
 
 def inline_keyboards(message):
-    list_of_commands = ['START', 'HELP', 'VALUES', 'POPULAR_CURRENCIES', 'TIMESERIES']
+    list_of_commands = ['START', 'HELP', 'VALUES', 'MOST_USED_PAIRS']
     button_list = []
     for command in list_of_commands:
         button_list.append(telebot.types.InlineKeyboardButton(command, callback_data='/'+command.lower()))
@@ -53,14 +48,9 @@ def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
 @bot.callback_query_handler(func=lambda c: c.data)
 def process_callback(callback_query: telebot.types.CallbackQuery):
     code = callback_query.data
+    logger.info(f"{code} button is pressed by user {callback_query.from_user.username}")
     if code in ['/start', '/help']:
-        text = f"HELLO, {callback_query.from_user.username}!\n" \
-               "I'm ChatBot where You can convert currencies!\n\n" \
-               "Insert currencies to exchange in following format (in one line): \n" \
-               "<currency to get> " \
-               "<currency to exchange> " \
-               "<amount to get>\n\n" \
-               "Example:\n\nEUR USD 1\n\n"
+        text = START_HELP_TEXT.format(callback_query.from_user.username)
         bot.send_message(callback_query.from_user.id, text=text)
     if code == '/values':
         text = "Available currencies:\n\n"
@@ -72,15 +62,7 @@ def process_callback(callback_query: telebot.types.CallbackQuery):
                 bot.send_message(callback_query.from_user.id, text=text[x:x + 4095])
         else:
             bot.send_message(callback_query.from_user.id, text)
-    if code == '/timeseries':
-        text = f"Insert dates to get timeseries data\n" \
-               f"in following format (in one line):\n" \
-               f"<date_from> <date_to> <currency quote> <currency base>\n\n" \
-               f"Example:\n\n" \
-               f"2023-02-01 2023-02-20 USD XAU\n"
-        mesg = bot.send_message(callback_query.from_user.id, text)
-        bot.register_next_step_handler(mesg, get_timeseries)
-    if code == '/popular_currencies':
+    if code == '/most_used_pairs':
         markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
         # markup = telebot.types.InlineKeyboardMarkup()
         if len(red.keys()) == 0:
@@ -95,94 +77,51 @@ def process_callback(callback_query: telebot.types.CallbackQuery):
             pairs.sort(reverse=True)
             buttons = [telebot.types.KeyboardButton(pair[1] + " 1") for pair in pairs]
             markup.add(*buttons[:6])
-        bot.send_message(callback_query.from_user.id, "Popular currencies", reply_markup=markup)
+        bot.send_message(callback_query.from_user.id, "Popular exchange pairs", reply_markup=markup)
+    logger.info(f"Answer is sent to user {callback_query.from_user.username}")
     inline_keyboards(callback_query)
-
-
-def get_timeseries(message: telebot.types.Message):
-    date_from, date_to, quote, base = message.text.split()
-    data = CryptoConverter.get_timeseries(date_from, date_to, quote, base)
-    df = pd.DataFrame(data=list(data.get('rates').values()))
-    df.columns = [base]
-    df["date"] = list(data.get('rates').keys())
-    df["date"] = df.date.apply(pd.to_datetime)
-    df.set_index('date', inplace=True)
-    ax = df.plot(kind='line', use_index=True, title=f"{base}/{quote}", grid=True, legend=True)
-    ax.figure.savefig('output.png')
-    bot.send_photo(message.from_user.id, photo=open('output.png', 'rb'))
-
-
-# @bot.message_handler(commands=["values"])
-# def handle_values(message: telebot.types.Message):
-#     text = "Available currencies:\n\n"
-#     global symbols
-#     for symbol, info in symbols.items():
-#         text += symbol + f' - {info.get("description")}\n'
-#     if len(text) > 4095:
-#         for x in range(0, len(text), 4095):
-#             mesg = bot.reply_to(message, text=text[x:x + 4095])
-#     else:
-#         mesg = bot.reply_to(message, text)
-#     bot.send_message(message.from_user.id, text)
-#     inline_keyboards(message)
-#     #bot.register_next_step_handler(mesg, inline_keyboards())
-
-
-# @bot.message_handler(commands=["timeseries_data"])
-# def handle_values(message: telebot.types.Message):
-#     text = f"Insert dates to get timeseries data\n" \
-#            f"in following format (in one line):\n" \
-#            f"<date_from> <date_to> <currency quote> <currency base>\n\n" \
-#            f"Example:\n\n" \
-#            f"2023-02-01 2023-02-20 USD XAU\n"
-#     mesg = bot.send_message(message.from_user.id, text)
-#     bot.register_next_step_handler(mesg, get_timeseries)
-
-
-# @bot.message_handler(commands=["popular_currencies"])
-# def get_popular_currencies(message: telebot.types.Message):
-#     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-#     # markup = telebot.types.InlineKeyboardMarkup()
-#     if len(red.keys()) == 0:
-#         bot.reply_to(message, "You have not used yet any.\nTry for example:\n")
-#         buttons = [telebot.types.KeyboardButton("EUR USD 1"), telebot.types.KeyboardButton("USD RUB 1")]
-#         markup.add(*buttons)
-#     if 0 < len(red.keys()) <= 6:
-#         buttons = [telebot.types.KeyboardButton(key.decode() + " 1") for key in red.keys()]
-#         markup.add(*buttons)
-#     if len(red.keys()) > 6:
-#         pairs = [(int((red.get(key).decode())), key.decode()) for key in red.keys()]
-#         pairs.sort(reverse=True)
-#         buttons = [telebot.types.KeyboardButton(pair[1] + " 1") for pair in pairs]
-#         markup.add(*buttons[:6])
-#     bot.send_message(message.from_user.id, "Popular currencies", reply_markup=markup)
-#     inline_keyboards(message)
 
 
 @bot.message_handler(content_types=["text"])
 def convert(message: telebot.types.Message):
     global symbols
     try:
-        values = message.text.split()
-        if len(values) != 3:
-            raise APIException("Wrong parameters quantity.")
-        quote, base, amount = values
-        price = CryptoConverter.get_price(quote, base)
+        parse_result = parse_message_text(message.text.strip())
+        logger.info(f"User {message.from_user.username} query message is: {message.text}, "
+                    f"the parse result is {parse_result}")
+        if parse_result == "wrong_query":
+            logger.error(APIException("Wrong parameters."))
+            raise APIException("Wrong parameters.")
+
+        values = message.text.strip().split()
+        if parse_result == "exchange":
+            quote, base, amount = values
+            logger.info(f"User {message.from_user.username} query to exchange: {quote} {base} {amount}")
+            price = CryptoConverter.get_price(quote, base)
+            text = f"Pricing {amount} " \
+                   f"{symbols.get(quote)['description']} ({quote}) in " \
+                   f"{symbols.get(base)['description']} ({base}) - {round(price * int(amount), 2)}"
+            bot.send_message(message.chat.id, text)
+            logger.info(f"Answer was sent to user {message.from_user.username}: {text}")
+            used_pairs = red.get(f"{quote} {base}")
+            if not used_pairs:
+                red.set(f"{quote} {base}", 1)
+            else:
+                red.set(f"{quote} {base}", int(used_pairs.decode()) + 1)
+            logger.info("Redis base is updated")
+        if parse_result == "timeseries":
+            date_from, date_to, quote, base = values
+            logger.info(f"User {message.from_user.username} query to exchange: {date_from} {date_to} {quote} {base}")
+            CryptoConverter.get_timeseries(date_from, date_to, quote, base)
+            bot.send_photo(message.from_user.id, photo=open('output.png', 'rb'))
+            logger.info(f"Image was sent to user {message.from_user.username}.")
+            inline_keyboards(message)
     except APIException as e:
+        logger.error(APIException(f"User mistake. {e}"))
         bot.reply_to(message, f"User mistake.\n{e}")
     except Exception as e:
+        logger.error(APIException(f"Wrong command. Unable to continue. {e}"))
         bot.reply_to(message, f"Wrong command. Unable to continue.\n{e}")
-    else:
-        text = f"Pricing {amount} " \
-               f"{symbols.get(quote)['description']} ({quote}) in " \
-               f"{symbols.get(base)['description']} ({base}) - {round(price * int(amount), 2)}"
-        bot.send_message(message.chat.id, text)
-
-        used_pairs = red.get(f"{quote} {base}")
-        if not used_pairs:
-            red.set(f"{quote} {base}", 1)
-        else:
-            red.set(f"{quote} {base}", int(used_pairs.decode()) + 1)
 
 
 bot.polling(none_stop=True, interval=0)
--- extensions.py
+++ extensions.py
@@ -1,12 +1,19 @@
 import requests
 import json
-from config import APILAYER_API_KEY, OPENEXCHANGERATES_API
+from config import APILAYER_API_KEY
+from exceptions import APIException
 from pprint import pprint
+import re
+import pandas as pd
 
 
-
-class APIException(Exception):
-    pass
+def parse_message_text(values: str) -> str:
+    if re.match("([A-Z]{3} ?){2}\d+", values):
+        return "exchange"
+    elif re.match("(\d{4}-\d{2}-\d{2} ){2}([A-Z]{3} ?){2}", values):
+        return "timeseries"
+    else:
+        return "wrong_query"
 
 
 class CryptoConverter:
@@ -17,9 +24,9 @@ class CryptoConverter:
     @staticmethod
     def get_price(quote: str, base: str):
         if quote == base:
-            raise APIException(f"Impossible to exchange equal currencies ({quote} vs {base})")
+            raise APIException(f"Impossible to exchange equal currencies ({quote.upper()} vs {base.upper()})")
 
-        URL = f"https://api.exchangerate.host/convert?from={quote}&to={base}"  # popular_currencies
+        URL = f"https://api.exchangerate.host/convert?from={quote.upper()}&to={base.upper()}"  # popular_currencies
 
         r = requests.get(URL)
         # print(json.loads(r.content))
@@ -56,20 +63,29 @@ class CryptoConverter:
     @staticmethod
     def get_timeseries(date_from: str, date_to: str, quote: str, base: str = "USD"):
         url = f'https://api.exchangerate.host/timeseries?' \
-              f'base={base}&' \
-              f'symbols={quote}&' \
+              f'base={base.upper()}&' \
+              f'symbols={quote.upper()}&' \
               f'start_date={date_from}&' \
               f'end_date={date_to}'
         response = requests.get(url)
+        #print(response.status_code)
         data = response.json()
-        return data
+        pprint(data)
+        #return data
+        df = pd.DataFrame(data=list(data.get('rates').values()))
+        df.columns = [base]
+        df["date"] = list(data.get('rates').keys())
+        df["date"] = df.date.apply(pd.to_datetime)
+        df.set_index('date', inplace=True)
+        ax = df.plot(kind='line', use_index=True, title=f"{base}/{quote}", grid=True, legend=True)
+        ax.figure.savefig('output.png')
 
 
 
 if __name__ == "__main__":
     # pprint(Currency.get_all_currencies())
     # price = CryptoConverter.get_price("USD", "RUB")
-    CryptoConverter.get_timeseries('2023-02-01', '2023-02-20', 'USD', 'XAU')
+    CryptoConverter.get_timeseries('2052-03-07', '2023-03-05', 'USD', 'AUD')
     #pprint(list(time_series.get('rates').keys()))
     #pprint(list(time_series.get('rates').values()))
 
